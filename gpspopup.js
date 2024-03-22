8696f1aabbec93af944ec90ff5dcfe26653d2fdc
Jack <zeljkok@shaw.ca>
2024-03-22 15:10:19 -0600

    Add files via upload

---
+++ gpspopup.js
@@ -0,0 +1,19 @@
+
+
+function gpstrack(gpslink)
+{
+   let url = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+   
+   fetch(url)
+    .then(response =>
+    {
+        window.open(gpslink, '_blank');
+    })
+    .catch(error => 
+    {
+        alert ('Please disable Ad Blocker to View Google Map or Download GPS Track');
+    });
+
+}
+
+
