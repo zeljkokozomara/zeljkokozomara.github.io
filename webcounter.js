2f9a3e27a81cf422241584a0acb671f5a6e94bfe
Jack <zeljkok@shaw.ca>
2026-04-14 18:29:36 -0600

    Create webcounter.js

---
+++ webcounter.js
@@ -0,0 +1,3 @@
+
+<script type="text/javascript" src="https://cdn.livetrafficfeed.com/static/v5/live.js?bc=ffffff&tc=000000&brd1=2853a8&lnk=135d9e&hc=ffffff&hfc=2853a8&nc=19ff19&vv=210&tft=10&ro=0&tz=America%2FNew_York&res=0"></script>
+<noscript id="LTF_live_website_visitor"><a href="http://livetrafficfeed.com">Website Online Counter</a></noscript>
